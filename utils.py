a98550b7606fc7bdb01a69393932830f61125092
bmuscato <158041174+bmuscato@users.noreply.github.com>
2025-04-10 14:29:51 +0200

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,122 +1,58 @@
-import pandas as pd
+from tqdm import tqdm
 
 
-def verify_filtering(df):
-    # Check for any rows that satisfy both conditions
+def generate_prompt(query, title, doc):
+    prompt = f'''You're an annotator chosen for a task of annotating the documents retrieved in response to the queries about controversial queries that we issued to the search engines, Bing and Google. 
+    The documents you will annotate have been chosen from the top-10 search results retrieved from these search engines. 
+    You're allowed to read the query and the corresponding document, then annotate the document with respect to the given query first as relevant or not-relevant, then if the document is relevant, you should annotate the document as pro, neutral, or against.
+
+    Pro: when the document is in favor of the controversial topic. The document describes more the pro aspects of the topic; 
+
+    Neutral: when the document does not support or help either side of the controversial topic. The document provides an impartial (fair) description of the pros and cons of the subject;
+
+    Against: when the document is against the controversial topic. The document describes more the cons aspects of the topic;
     
-    condition = (df['majority_label'] == 'No majority') | (df['If there is a link-broken option'] == 1)
-    
-    if df.loc[condition].empty:
-        return "Verification passed: No rows with 'No majority' and link broken are present."
-    else:
-        return "Verification failed: There are still rows with 'No majority' and link broken."
+    Not-relevant: when the document is irrelevant regarding the controversial topic;
+
+    QUERY:
+
+    {query}
+
+    DOCUMENT TITLE:
+
+    {title}
+
+    DOCUMENT:
+
+    {doc}
+    '''
+    return prompt
 
 
-def clean_data(df):
-    df = df.loc[~((df['majority_label'] == 'No majority') | (df['If there is a link-broken option'] == 1))]
-    return df
-
-
-def doc_cocat(row):
-    if row['gpt_summaries'].strip() != '':
-        return row['gpt_summaries']
-    return row['docCont']
-
-
-def combine_columns(df):
+def annotate_dataframe(df, generator, col_name, output_file=None):
     """
-    Combine 'Query', 'docTitle', and 'doc' columns into a new 'Input' column.
-
-    Parameters:
-    df (pd.DataFrame): DataFrame containing 'Query', 'docTitle', and 'doc' columns.
-
-    Returns:
-    pd.DataFrame: DataFrame with a new 'Input' column.
-    """
-    df['Input'] = df['Query'] + ' ' + df['docTitle'] + ' ' + df['doc']
-    return df
-
-
-def baseline_data(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Process the DataFrame for the baseline dataset.
-    
-    This function combines the answers from columns 'answer1', 'answer2', and 'answer3' 
-    into a list for each row, storing them in a new 'labels' column. The resulting 
-    DataFrame includes only 'docID', 'Input', 'labels', and 'majority_label' columns.
+    Annotates the DataFrame by generating prompts and corresponding generated text.
     
     Parameters:
-    df (pd.DataFrame): The input DataFrame containing the original data.
-
-    Returns:
-    pd.DataFrame: The processed DataFrame with the specified columns.
-    """
-    df['labels'] = df[['answer1', 'answer2', 'answer3']].apply(lambda x: x.dropna().tolist(), axis=1)
-    df = df[['docID', 'Query','docTitle', 'doc', 'Input', 'labels', 'majority_label']]
-    return df
-
-
-def multip_data(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Process the DataFrame for the multiperspective dataset.
+    df (pd.DataFrame): The DataFrame containing 'Query', 'docTitle', and 'doc' columns.
+    generator (function): The text generation function to apply to each prompt.
+    output_file (str): Optional. The file path to save the annotated DataFrame as a CSV.
     
-    This function combines the answers from columns 'answer1', 'answer2', and 'answer3' 
-    into a list, then explodes the list into separate rows for each answer, effectively 
-    creating multiple rows for each original input. It also removes any rows where 
-    the 'label' is 'Link-broken'.
+    Returns:
+    pd.DataFrame: The annotated DataFrame with 'Prompt' and 'GeneratedText' columns added.
+    """
     
-    Parameters:
-    df (pd.DataFrame): The input DataFrame containing the original data.
-
-    Returns:
-    pd.DataFrame: The processed DataFrame with exploded labels.
-    """
-    df['label'] = df[['answer1', 'answer2', 'answer3']].apply(lambda x: x.dropna().tolist(), axis=1)
-    df = df.explode('label').reset_index(drop=True)
-    df = df[['docID', 'Query' ,'docTitle', 'doc', 'Input', 'label', 'majority_label']]
+    # Initialize tqdm progress bar for applying the function to rows
+    tqdm.pandas(desc="Annotating DataFrame")
+    
+    # Generate prompts based on the 'Query', 'docTitle', and 'doc' columns
+    df['Prompt'] = df.progress_apply(lambda row: generate_prompt(row['Query'], row['docTitle'], row['doc']), axis=1)
+    df['Prompt'] = df['Prompt'].str.replace('\n', ' ')
+    # Apply the text generation function to each prompt and store the results
+    df[col_name] = df['Prompt'].progress_apply(generator)
+    
+    # Optionally save the DataFrame to a CSV file
+    if output_file:
+        df.to_csv(output_file, index=False)
+    
     return df
-
-
-def save_dataset(df_train: pd.DataFrame, df_test: pd.DataFrame, df_val: pd.DataFrame, output_dir: str, choice: str) -> None:
-    """
-    Save the processed DataFrames for train, test, and val splits to a specified output directory 
-    based on the chosen dataset type.
-
-    This function processes each DataFrame (train, test, val) using either the `baseline_data` or 
-    `multip_data` function, and then saves each processed DataFrame to the specified output directory 
-    with filenames that reflect both the dataset type and the split (train, test, or val).
-
-    Parameters:
-    df_train (pd.DataFrame): The DataFrame containing the train data.
-    df_test (pd.DataFrame): The DataFrame containing the test data.
-    df_val (pd.DataFrame): The DataFrame containing the validation data.
-    output_dir (str): The directory where the processed DataFrames will be saved.
-    choice (str): The type of dataset to generate ('baseline' or 'multip').
-
-    Returns:
-    None
-    """
-    # Process each DataFrame based on the choice
-    if choice == 'baseline':
-        processed_train = baseline_data(df_train)
-        processed_test = baseline_data(df_test)
-        processed_val = baseline_data(df_val)
-    elif choice == 'multip':
-        processed_train = multip_data(df_train)
-        processed_test = multip_data(df_test)
-        processed_val = multip_data(df_val)
-    else:
-        raise ValueError("Invalid choice! Use 'baseline' or 'multip'.")
-
-    # Print the shapes of the processed DataFrames
-    print(f"Train DataFrame shape: {processed_train.shape}")
-    print(f"Test DataFrame shape: {processed_test.shape}")
-    print(f"Val DataFrame shape: {processed_val.shape}")
-
-    # Construct and save each DataFrame to the appropriate file path
-    processed_train.to_csv(f"{output_dir}/train_{choice}.csv", index=False)
-    processed_test.to_csv(f"{output_dir}/test_{choice}.csv", index=False)
-    processed_val.to_csv(f"{output_dir}/val_{choice}.csv", index=False)
-
-    # Confirm the files have been saved
-    print(f"Train, Test, and Val datasets for '{choice}' saved to {output_dir}")
